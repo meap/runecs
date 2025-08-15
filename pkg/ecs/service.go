2cb13d295a6640f1ad2429c4ed8c31382607c3d0
Petr Reichl <petr@tapmates.com>
2025-08-15 09:04:07 +0800

    ♻️ refactor: consolidate ECS commands into service.go and remove legacy files
    - Remove deploy.go, exec.go, logs.go, prune.go, restart.go, revision.go
    - Migrate ECS logic into pkg/ecs/service.go
    - Net: 630 insertions, 655 deletions across 7 files

--- pkg/ecs/service.go
+++ pkg/ecs/service.go
@@ -16,12 +16,22 @@ package ecs
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"log"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/retry"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
 	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
+	"github.com/dustin/go-humanize"
+	"github.com/fatih/color"
+	"github.com/jinzhu/copier"
+	"github.com/rodaine/table"
 )
 
 const (
@@ -33,6 +43,29 @@ type Service struct {
 	Cluster string `mapstructure:"CLUSTER"`
 	Service string `mapstructure:"SERVICE"`
 }
+
+// Local types used by Service methods
+type serviceDefinition struct {
+	Subnets        []string
+	SecurityGroups []string
+	TaskDef        string
+}
+
+type taskDefinition struct {
+	Name            string
+	LogGroup        string
+	LogStreamPrefix string
+	Cpu             string
+	Memory          string
+}
+
+type printProcessLogsOutput struct {
+	lastEventTimestamp int64
+}
+
+// =============================================================================
+// Core Service Methods
+// =============================================================================
 
 func (s *Service) loadService(ctx context.Context, svc *ecs.Client) (types.Service, error) {
 	response, err := svc.DescribeServices(ctx, &ecs.DescribeServicesInput{
@@ -61,4 +94,601 @@ func initCfg() (aws.Config, error) {
 	}
 
 	return cfg, nil
+}
+
+// =============================================================================
+// Deploy Methods
+// =============================================================================
+
+func (s *Service) cloneTaskDef(ctx context.Context, dockerImageTag string, svc *ecs.Client) (string, error) {
+	_, err := s.loadService(ctx, svc)
+	if err != nil {
+		return "", err
+	}
+
+	// Get the last task definition ARN.
+	// Load the latest task definition.
+	latestDef, err := s.latestTaskDefinitionArn(ctx, svc)
+	if err != nil {
+		return "", err
+	}
+
+	response, err := svc.DescribeTaskDefinition(ctx, &ecs.DescribeTaskDefinitionInput{
+		TaskDefinition: &latestDef,
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	if len(response.TaskDefinition.ContainerDefinitions) > 1 {
+		return "", errors.New("multiple container definitions in a single task are not supported")
+	}
+
+	newDef := &ecs.RegisterTaskDefinitionInput{}
+	if err := copier.Copy(newDef, response.TaskDefinition); err != nil {
+		return "", err
+	}
+
+	split := strings.Split(*newDef.ContainerDefinitions[0].Image, ":")
+	newDockerURI := fmt.Sprintf("%s:%s", split[0], dockerImageTag)
+
+	newDef.ContainerDefinitions[0].Image = &newDockerURI
+
+	output, err := svc.RegisterTaskDefinition(ctx, newDef)
+	if err != nil {
+		return "", err
+	}
+
+	return *output.TaskDefinition.TaskDefinitionArn, nil
+}
+
+func (s *Service) Deploy(dockerImageTag string) {
+	cfg, err := initCfg()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	ctx := context.Background()
+
+	svc := ecs.NewFromConfig(cfg)
+
+	// Clones the latest version of the task definition and inserts the new Docker URI.
+	taskDefinitionArn, err := s.cloneTaskDef(ctx, dockerImageTag, svc)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("New revision of the task %s has been created\n", taskDefinitionArn)
+
+	updateOutput, err := svc.UpdateService(ctx, &ecs.UpdateServiceInput{
+		Cluster:        &s.Cluster,
+		Service:        &s.Service,
+		TaskDefinition: &taskDefinitionArn,
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("Service %s has been updated.\n", *updateOutput.Service.ServiceArn)
+}
+
+// =============================================================================
+// Execute Methods
+// =============================================================================
+
+func (s *Service) describeService(ctx context.Context, client *ecs.Client) (serviceDefinition, error) {
+	resp, err := client.DescribeServices(ctx, &ecs.DescribeServicesInput{
+		Cluster:  &s.Cluster,
+		Services: []string{s.Service},
+	})
+
+	if err != nil {
+		return serviceDefinition{}, err
+	}
+
+	def := resp.Services[0]
+	fmt.Printf("Service '%s' loaded. \n", *def.ServiceName)
+
+	// Fetch the latest task definition. Keep in mind that the active service may
+	// have a different task definition that is available, see. *def.TaskDefinition
+	taskDef, err := s.latestTaskDefinitionArn(ctx, client)
+	if err != nil {
+		return serviceDefinition{}, err
+	}
+
+	return serviceDefinition{
+		Subnets:        def.NetworkConfiguration.AwsvpcConfiguration.Subnets,
+		SecurityGroups: def.NetworkConfiguration.AwsvpcConfiguration.SecurityGroups,
+		TaskDef:        taskDef,
+	}, nil
+}
+
+func (s *Service) describeTask(ctx context.Context, client *ecs.Client, taskArn *string) (taskDefinition, error) {
+	resp, _ := client.DescribeTaskDefinition(ctx, &ecs.DescribeTaskDefinitionInput{TaskDefinition: taskArn})
+
+	if len(resp.TaskDefinition.ContainerDefinitions) == 0 {
+		return taskDefinition{}, fmt.Errorf("no container definitions found in task definition %s", *taskArn)
+	}
+
+	output := taskDefinition{}
+	output.Name = *resp.TaskDefinition.ContainerDefinitions[0].Name
+
+	logConfig := resp.TaskDefinition.ContainerDefinitions[0].LogConfiguration
+	if logConfig != nil && logConfig.LogDriver == types.LogDriverAwslogs {
+		output.LogGroup = logConfig.Options["awslogs-group"]
+		output.LogStreamPrefix = logConfig.Options["awslogs-stream-prefix"]
+	}
+
+	output.Cpu = *resp.TaskDefinition.Cpu
+	output.Memory = *resp.TaskDefinition.Memory
+
+	return output, nil
+}
+
+func (s *Service) wait(ctx context.Context, client *ecs.Client, task string) (bool, error) {
+	output, err := client.DescribeTasks(ctx, &ecs.DescribeTasksInput{
+		Cluster: &s.Cluster,
+		Tasks:   []string{task},
+	})
+
+	if err != nil {
+		return false, err
+	}
+
+	if *output.Tasks[0].LastStatus == "STOPPED" {
+		if exitCode := *output.Tasks[0].Containers[0].ExitCode; exitCode != 0 {
+			return true, fmt.Errorf("task %s failed with exit code %d", *output.Tasks[0].TaskArn, exitCode)
+		}
+	}
+
+	return *output.Tasks[0].LastStatus == "STOPPED", nil
+}
+
+func (s *Service) Execute(cmd []string, wait bool, dockerImageTag string) {
+	cfg, err := initCfg()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	ctx := context.Background()
+
+	svc := ecs.NewFromConfig(cfg)
+
+	sdef, err := s.describeService(ctx, svc)
+	if err != nil {
+		log.Fatalf("An error occurred while loading the service %s in the cluster %s: %v", s.Service, s.Cluster, err)
+	}
+
+	tdef, err := s.describeTask(ctx, svc, &sdef.TaskDef)
+	if err != nil {
+		log.Fatalf("An error occurred while loading the task definition %s: %v", sdef.TaskDef, err)
+	}
+
+	var taskDef string
+
+	if dockerImageTag != "" {
+		taskDef, err = s.cloneTaskDef(ctx, dockerImageTag, svc)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Printf("New task definition %s is created", taskDef)
+	} else {
+		taskDef = sdef.TaskDef
+		fmt.Printf("The task definition %s is used", taskDef)
+	}
+
+	fmt.Println()
+
+	output, err := svc.RunTask(ctx, &ecs.RunTaskInput{
+		Cluster:        &s.Cluster,
+		TaskDefinition: &taskDef,
+		LaunchType:     "FARGATE",
+		NetworkConfiguration: &types.NetworkConfiguration{
+			AwsvpcConfiguration: &types.AwsVpcConfiguration{
+				Subnets:        sdef.Subnets,
+				SecurityGroups: sdef.SecurityGroups,
+				AssignPublicIp: "ENABLED",
+			},
+		},
+		Overrides: &types.TaskOverride{
+			ContainerOverrides: []types.ContainerOverride{{
+				Name:    &tdef.Name,
+				Command: cmd,
+			}},
+		},
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	executedTask := output.Tasks[0]
+	var lastTimestamp *int64 = nil
+
+	fmt.Printf("Task %s executed\n", *executedTask.TaskArn)
+
+	if wait {
+		for {
+			logsOutput, _ := s.printProcessLogs(ctx, tdef.LogGroup, tdef.LogStreamPrefix, *executedTask.TaskArn, tdef.Name, lastTimestamp)
+			lastTimestamp = &logsOutput.lastEventTimestamp
+
+			success, err := s.wait(ctx, svc, *executedTask.TaskArn)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			if success {
+				break
+			}
+
+			time.Sleep(5 * time.Second)
+		}
+
+		fmt.Printf("task %s finished\n", *executedTask.TaskArn)
+	}
+}
+
+// =============================================================================
+// Logs Methods
+// =============================================================================
+
+func (s *Service) printProcessLogs(
+	ctx context.Context, logGroupname string,
+	logStreamPrefix string,
+	taskArn string,
+	name string,
+	startTime *int64) (printProcessLogsOutput, error) {
+
+	cfg, err := initCfg()
+	if err != nil {
+		return printProcessLogsOutput{}, fmt.Errorf("failed to initialize AWS configuration. (%w)", err)
+	}
+
+	processID := extractProcessID(taskArn)
+	client := cloudwatchlogs.NewFromConfig(cfg)
+
+	input := &cloudwatchlogs.FilterLogEventsInput{
+		LogGroupName:   aws.String(logGroupname),
+		LogStreamNames: []string{fmt.Sprintf("%s/%s/%s", logStreamPrefix, name, processID)},
+	}
+
+	if startTime != nil {
+		input.StartTime = startTime
+	}
+
+	output, err := client.FilterLogEvents(ctx, input)
+
+	if err != nil {
+		return printProcessLogsOutput{}, fmt.Errorf("failed to filter log events (%w)", err)
+	}
+
+	for _, event := range output.Events {
+		fmt.Println(*event.LogStreamName, *event.Message)
+	}
+
+	var lastEventTimestamp int64
+	if len(output.Events) > 0 {
+		lastEventTimestamp = *output.Events[len(output.Events)-1].Timestamp + 1
+	} else if startTime != nil {
+		lastEventTimestamp = *startTime
+	} else {
+		lastEventTimestamp = 0
+	}
+
+	return printProcessLogsOutput{
+		lastEventTimestamp: lastEventTimestamp,
+	}, nil
+}
+
+// =============================================================================
+// Prune Methods
+// =============================================================================
+
+func (s *Service) deregisterTaskFamily(ctx context.Context, family string, keepLast int, keepDays int, dryRun bool, svc *ecs.Client) {
+	definitionInput := &ecs.ListTaskDefinitionsInput{
+		FamilyPrefix: &family,
+		Sort:         types.SortOrderDesc,
+	}
+
+	today := time.Now().UTC()
+	totalCount := 0
+	deleted := 0
+	keep := 0
+
+	for {
+		resp, err := svc.ListTaskDefinitions(ctx, definitionInput)
+		if err != nil {
+			log.Fatalln("Loading the list of definitions failed.")
+		}
+
+		count := len(resp.TaskDefinitionArns)
+		totalCount += count
+
+		for _, def := range resp.TaskDefinitionArns {
+			response, err := svc.DescribeTaskDefinition(ctx, &ecs.DescribeTaskDefinitionInput{
+				TaskDefinition: &def,
+			})
+
+			if err != nil {
+				log.Printf("Failed to describe task definition %s. (%v)\n", def, err)
+
+				continue
+			}
+
+			diffInDays := int(today.Sub(*response.TaskDefinition.RegisteredAt).Hours() / 24)
+
+			// Last X
+			if keep < keepLast {
+				fmt.Println("Task definition", def, "created", diffInDays, "days ago is skipped.")
+				keep++
+
+				continue
+			}
+
+			if diffInDays < keepDays {
+				fmt.Println("Task definition", def, "created", diffInDays, "days ago is skipped.")
+
+				continue
+			}
+
+			deleted++
+
+			if !dryRun {
+				_, err := svc.DeregisterTaskDefinition(ctx, &ecs.DeregisterTaskDefinitionInput{TaskDefinition: &def})
+				if err != nil {
+					fmt.Printf("Deregistering the task definition %s failed. (%v)\n", def, err)
+
+					continue
+				}
+
+				fmt.Println("Task definition", def, "created", diffInDays, "days ago is deregistered.")
+			}
+		}
+
+		if resp.NextToken == nil {
+			break
+		}
+
+		definitionInput.NextToken = resp.NextToken
+	}
+
+	if dryRun {
+		fmt.Printf("Total of %d task definitions. Will delete %d definitions.", totalCount, deleted)
+
+		return
+	}
+
+	fmt.Printf("Total of %d task definitions. Deleted %d definitions.", totalCount, deleted)
+}
+
+func (s *Service) Prune(keepLast int, keepDays int, dryRun bool) {
+	cfg, err := initCfg()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	ctx := context.Background()
+
+	svc := ecs.NewFromConfig(cfg)
+
+	familyPrefix, err := s.getFamilyPrefix(ctx, svc)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	response, err := s.getFamilies(ctx, familyPrefix, svc)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, family := range response {
+		s.deregisterTaskFamily(ctx, family, keepLast, keepDays, dryRun, svc)
+	}
+}
+
+// =============================================================================
+// Restart Methods
+// =============================================================================
+
+func (s *Service) stopAll(ctx context.Context, client *ecs.Client) error {
+	tasks, err := client.ListTasks(ctx, &ecs.ListTasksInput{
+		Cluster:     &s.Cluster,
+		ServiceName: &s.Service,
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, taskArn := range tasks.TaskArns {
+		output, err := client.StopTask(ctx, &ecs.StopTaskInput{
+			Cluster: &s.Cluster,
+			Task:    &taskArn,
+		})
+		if err != nil {
+			log.Println(fmt.Errorf("failed to stop task %s. (%w)", taskArn, err))
+
+			continue
+		}
+
+		fmt.Printf("Stopped task %s started %s\n", *output.Task.TaskArn, humanize.Time(*output.Task.StartedAt))
+	}
+
+	return nil
+}
+
+func (s *Service) forceNewDeploy(ctx context.Context, client *ecs.Client) error {
+	taskDef, err := s.latestTaskDefinitionArn(ctx, client)
+	if err != nil {
+		return err
+	}
+
+	output, err := client.UpdateService(ctx, &ecs.UpdateServiceInput{
+		Cluster:            &s.Cluster,
+		Service:            &s.Service,
+		TaskDefinition:     &taskDef,
+		ForceNewDeployment: true,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Service %s restarted by starting new tasks using the task definition %s.\n", s.Service, *output.Service.TaskDefinition)
+
+	return nil
+}
+
+func (s *Service) Restart(kill bool) {
+	cfg, err := initCfg()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	svc := ecs.NewFromConfig(cfg)
+	if kill {
+		err := s.stopAll(context.Background(), svc)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	} else {
+		err := s.forceNewDeploy(context.Background(), svc)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	fmt.Println("Done.")
+}
+
+// =============================================================================
+// Revision Methods
+// =============================================================================
+
+func (s *Service) getFamilies(ctx context.Context, familyPrefix string, svc *ecs.Client) ([]string, error) {
+	response, err := svc.ListTaskDefinitionFamilies(ctx, &ecs.ListTaskDefinitionFamiliesInput{
+		FamilyPrefix: &familyPrefix,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Families, nil
+}
+
+func (s *Service) getFamilyPrefix(ctx context.Context, svc *ecs.Client) (string, error) {
+	service, err := s.loadService(ctx, svc)
+	if err != nil {
+		return "", err
+	}
+
+	response, err := svc.DescribeTaskDefinition(ctx, &ecs.DescribeTaskDefinitionInput{
+		TaskDefinition: service.TaskDefinition,
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return *response.TaskDefinition.Family, nil
+}
+
+func (s *Service) latestTaskDefinitionArn(ctx context.Context, svc *ecs.Client) (string, error) {
+	_, err := s.loadService(ctx, svc)
+	if err != nil {
+		return "", err
+	}
+
+	prefix, err := s.getFamilyPrefix(ctx, svc)
+	if err != nil {
+		return "", err
+	}
+
+	response, err := svc.ListTaskDefinitions(ctx, &ecs.ListTaskDefinitionsInput{
+		FamilyPrefix: &prefix,
+		Sort:         types.SortOrderDesc,
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return response.TaskDefinitionArns[0], nil
+}
+
+func (s *Service) printRevisions(ctx context.Context, familyPrefix string, lastRevisionsNr int, svc *ecs.Client) {
+	definitionInput := &ecs.ListTaskDefinitionsInput{
+		FamilyPrefix: &familyPrefix,
+		Sort:         types.SortOrderDesc,
+	}
+
+	headerFmt := color.New(color.FgBlue, color.Underline).SprintfFunc()
+	columnFmt := color.New(color.FgYellow).SprintfFunc()
+
+	tbl := table.New("Revision", "Created At", "Docker URI")
+	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+
+	total := 0
+
+	for {
+		response, err := svc.ListTaskDefinitions(ctx, definitionInput)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		for _, def := range response.TaskDefinitionArns {
+			if lastRevisionsNr != 0 && lastRevisionsNr < (total+1) {
+				break
+			}
+
+			resp, err := svc.DescribeTaskDefinition(ctx, &ecs.DescribeTaskDefinitionInput{
+				TaskDefinition: &def,
+			})
+
+			if err != nil {
+				log.Printf("Failed to describe task definition %s. (%v)\n", def, err)
+
+				continue
+			}
+
+			tbl.AddRow(resp.TaskDefinition.Revision, resp.TaskDefinition.RegisteredAt, *resp.TaskDefinition.ContainerDefinitions[0].Image)
+			total++
+		}
+
+		if response.NextToken == nil {
+			break
+		}
+
+		definitionInput.NextToken = response.NextToken
+	}
+
+	tbl.Print()
+}
+
+func (s *Service) Revisions(lastRevisionNr int) {
+	cfg, err := initCfg()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	ctx := context.Background()
+
+	svc := ecs.NewFromConfig(cfg)
+
+	familyPrefix, err := s.getFamilyPrefix(ctx, svc)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	response, err := s.getFamilies(ctx, familyPrefix, svc)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, family := range response {
+		s.printRevisions(ctx, family, lastRevisionNr, svc)
+	}
 }
